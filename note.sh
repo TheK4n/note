54af4638dbe0f3e452a4d99eba89ea75d9d4cb99
TheK4n <djvlad967891@gmail.com>
2022-10-15 21:05:04 +0300

    add mv func

--- note.sh
+++ note.sh
@@ -7,22 +7,22 @@ bye() {
 
 cmd_usage() {
     echo 'Usage:
-    tip init
-        Initialize new tip storage
-    tip ls
+    note init
+        Initialize new note storage
+    note ls
         List tips
-    tip help
-        Show this text
-    tip edit (tip-name)
-        Creates or edit existing tip with $EDITOR, after save changes by git
-    tip show (tip-name)
-        Render tip in terminal by glow
-    tip render (tip-name)
-        Render tip in browser by grip in localhost:6751
-    tip rm (tip-name)
-        Removes tip
-    tip mv (tip-name) (new-tip-name)
-        Rename tip'
+    note edit (note-name)
+        Creates or edit existing note with $EDITOR, after save changes by git
+    note show (note-name)
+        Render note in terminal by glow
+    note render (note-name)
+        Render note in browser by grip in localhost:6751
+    note rm (note-name)
+        Removes note
+    note mv (note-name) (new-note-name)
+        Rename note
+    note help
+        Show this text'
 }
 
 cmd_init() {
@@ -31,9 +31,8 @@ cmd_init() {
     git init "$PREFIX"
 }
 
-git_add_commit() {
-    git -C "$PREFIX" add "$1"
-    git_commit "$2"
+git_add() {
+    git -C "$PREFIX" add "$PREFIX/$1"
 }
 
 git_commit() {
@@ -42,12 +41,13 @@ git_commit() {
 
 cmd_edit() {
     last_modified_time="$(stat -c '%Y' "$PREFIX/$1")"
-    test -n "$1" || bye "No tip name provided"
+    test -n "$1" || bye "No note name provided"
     $EDITOR "$PREFIX/$1"
 
     if [ "$last_modified_time" != "$(stat -c '%Y' "$PREFIX/$1")" ]; then
-        git_add_commit "$PREFIX/$1" "Edited tip $1"
-        echo "Edited tip "$PREFIX/$1""
+        git_add "$1"
+        git_commit "Edited note $1"
+        echo "Edited note "$PREFIX/$1""
     fi
 }
 
@@ -56,23 +56,35 @@ cmd_list() {
 }
 
 cmd_show() {
-    test -n "$1" || bye "No tip name provided"
-    test -e "$PREFIX/$1" || bye "No tip in $PREFIX"
+    test -n "$1" || bye "No note name provided"
+    test -e "$PREFIX/$1" || bye "No note in $PREFIX"
     glow -p "$PREFIX/$1"
 }
 
 cmd_render() {
-    test -n "$1" || bye "No tip name provided"
-    test -e "$PREFIX/$1" || bye "No tip in $PREFIX"
+    test -n "$1" || bye "No note name provided"
+    test -e "$PREFIX/$1" || bye "No note in $PREFIX"
     echo "http://localhost:6751 in browser"
     grip -b "$PREFIX/$1" localhost:6751 1>/dev/null 2>/dev/null
 }
 
 cmd_delete() {
-    test -n "$1" || bye "No tip name provided"
-    test -e "$PREFIX/$1" || bye "No tip in $PREFIX"
+    test -n "$1" || bye "No note name provided"
+    test -e "$PREFIX/$1" || bye "No note in $PREFIX"
     rm "$PREFIX/$1"
-    git_add_commit "$PREFIX/$1" "Removed tip $1"
+    git_add "$1"
+    git_commit "Removed note $1"
+}
+
+cmd_rename() {
+    test -n "$1" || bye "No note name provided"
+    test -e "$PREFIX/$1" || bye "No note $1 in $PREFIX"
+    test -n "$2" || bye "No new note name provided"
+    test -e "$PREFIX/$2" && bye "Note $2 already exists"
+    mv "$PREFIX/$1" "$PREFIX/$2"
+    git_add "$1"
+    git_add "$2"
+    git_commit "Note $1 renamed to $2"
 }
 
 case "$1" in
@@ -83,6 +95,7 @@ case "$1" in
     ls) shift;                 cmd_list  "$@" ;;
     edit) shift;               cmd_edit  "$@" ;;
     rm) shift;                 cmd_delete  "$@" ;;
+    mv) shift;                 cmd_rename  "$@" ;;
 
     *)                         cmd_list    "$@" ;;
 esac
